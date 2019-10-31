20511166f48d2f903443ac87b47682889c3fe331
Johannes Hartwig <johannes.hartwig@uni-bayreuth.de>
2019-10-31 14:16:49 +0100

    stub for motion recorder; bugfix in controller

---
+++ source/franka_proxy/franka_motion_recorder.cpp
@@ -0,0 +1,29 @@
+/**
+ *************************************************************************
+ *
+ * @file franka_motion_recorder.cpp
+ *
+ * ..., implementation.
+ *
+ ************************************************************************/
+
+#include "franka_motion_recorder.hpp"
+
+
+namespace franka_proxy
+{
+namespace detail
+{
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+// franka_motion_recorder
+//
+//////////////////////////////////////////////////////////////////////////
+motion_recorder::motion_recorder(double rate)
+{ }
+
+
+} /* namespace detail */
+} /* namespace franka_proxy */
---
+++ source/franka_proxy/franka_motion_recorder.hpp
@@ -0,0 +1,41 @@
+/**
+ *************************************************************************
+ *
+ * @file franka_motion_recorder.hpp
+ * 
+ * todo
+ *
+ ************************************************************************/
+
+
+#if !defined(INCLUDED__FRANKA_PROXY__FRANKA_MOTION_RECORDER_HPP)
+#define INCLUDED__FRANKA_PROXY__FRANKA_MOTION_RECORDER_HPP
+
+
+namespace franka_proxy
+{
+namespace detail
+{
+
+
+/**
+ *************************************************************************
+ *
+ * @class motion_recorder
+ *
+ * todo
+ *
+ ************************************************************************/
+class motion_recorder
+{
+public:
+	motion_recorder
+		(double rate);
+};
+
+
+} /* namespace detail */
+} /* namespace franka_proxy */
+
+
+#endif /* !defined(INCLUDED__FRANKA_PROXY__FRANKA_MOTION_RECORDER_HPP) */
