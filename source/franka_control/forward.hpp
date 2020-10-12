1b7af389f0b9ff85613a79f32276bf809f98d317
David Harrer <david.harrer@uni-bayreuth.de>
2020-10-12 16:50:43 +0200

    Add forward headers.

---
+++ source/franka_control/forward.hpp
@@ -0,0 +1,30 @@
+/**
+ *************************************************************************
+ *
+ * @file forward.hpp
+ *
+ * Forward declarations for all franka_control types.
+ *
+ ************************************************************************/
+
+
+#if !defined(INCLUDED__FRANKA_CONTROL__FORWARD_HPP)
+#define INCLUDED__FRANKA_CONTROL__FORWARD_HPP
+
+
+namespace franka_control
+{
+
+
+class franka_controller;
+
+class franka_controller_emulated;
+class franka_controller_remote;
+
+
+
+
+} /* namespace franka_control */
+
+
+#endif /* !defined(INCLUDED__FRANKA_CONTROL__FORWARD_HPP) */
---
+++ source/franka_proxy_client/forward.hpp
@@ -0,0 +1,30 @@
+/**
+ *************************************************************************
+ *
+ * @file forward.hpp
+ *
+ * Forward declarations for all franka_proxy_client types.
+ *
+ ************************************************************************/
+
+
+#if !defined(INCLUDED__FRANKA_PROXY_CLIENT__FORWARD_HPP)
+#define INCLUDED__FRANKA_PROXY_CLIENT__FORWARD_HPP
+
+
+namespace franka_proxy
+{
+
+
+class franka_state_client;
+class franka_control_client;
+
+class franka_remote_controller;
+
+
+
+
+} /* namespace franka_proxy */
+
+
+#endif /* !defined(INCLUDED__FRANKA_PROXY_CLIENT__FORWARD_HPP) */
