fa3907aa95872ef273f5a9455f846423ceb451ad
David Harrer <david.harrer@uni-bayreuth.de>
2021-02-15 17:09:13 +0100

    Added missing threads library to cmake

--- cmake/franka-proxyConfig.cmake
+++ cmake/franka-proxyConfig.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(asio CONFIG)
 find_dependency(Eigen3 CONFIG)
+find_dependency(Threads REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/franka-proxyTargets.cmake")
