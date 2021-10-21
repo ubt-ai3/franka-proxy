d018e7a38c411fa796fc83a2508b1452fd6dafc3
David Singer <david.singer@uni-bayreuth.de>
2021-10-21 10:27:24 +0200

    Fixed wrong include name

--- cmake/franka-proxyConfig.cmake
+++ cmake/franka-proxyConfig.cmake
@@ -1,7 +1,7 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(asio CONFIG)
-find_dependency(nlohmann-json CONFIG)
+find_dependency(nlohmann_json CONFIG)
 find_dependency(Eigen3 CONFIG)
 find_dependency(Threads REQUIRED)
 
