d71bd3c6be8e2caa89714a56a1bd6ae5feed1e6e
Johannes Hartwig <johannes.hartwig@uni-bayreuth.de>
2024-01-25 18:41:41 +0100

    formatting for client test

--- source/franka_proxy_client_test/main.cpp
+++ source/franka_proxy_client_test/main.cpp
@@ -40,19 +40,19 @@ void franka_proxy_client_test(const std::string& ip)
 	std::atomic_bool stop(false);
 	std::thread t
 	([&stop, &robot, print_every_ith_status]()
+	{
+		int i = 0;
+		while (!stop)
 		{
-			int i = 0;
-			while (!stop)
-			{
-				robot.update();
+			robot.update();
 
-				if (print_every_ith_status && i++ % print_every_ith_status == 0)
-					print_status(robot);
+			if (print_every_ith_status && i++ % print_every_ith_status == 0)
+				print_status(robot);
 
-				using namespace std::chrono_literals;
-				std::this_thread::sleep_for(0.016s);
-			}
-		});
+			using namespace std::chrono_literals;
+			std::this_thread::sleep_for(0.016s);
+		}
+	});
 
 	// --- franka motion tests ---
 	//gripper_test(robot);
@@ -109,7 +109,7 @@ template <class Function> void execute_retry(Function&& f, franka_proxy::franka_
 
 void ple_motion_record(franka_proxy::franka_remote_interface& robot)
 {
-	franka_proxy::robot_config_7dof sp{ {0.0346044, -0.0666144, -0.0398886, -2.04985, -0.0229875, 1.99782, 0.778461} };
+	franka_proxy::robot_config_7dof sp{{0.0346044, -0.0666144, -0.0398886, -2.04985, -0.0229875, 1.99782, 0.778461}};
 	robot.move_to(sp);
 	std::this_thread::sleep_for(std::chrono::seconds(3));
 	robot.ple_motion(10.0, true);
@@ -139,20 +139,22 @@ void playback_test(franka_proxy::franka_remote_interface& robot)
 
 
 	robot.move_to(record.first.front());
-	const std::vector<std::array<double, 6>> selection_vectors(record.second.size(), std::array<double, 6>{1, 1, 1, 1, 1, 1});
+	const std::vector<std::array<double, 6>> selection_vectors(record.second.size(),
+	                                                           std::array<double, 6>{1, 1, 1, 1, 1, 1});
 	robot.move_sequence(record.first, record.second, selection_vectors);
 
 	std::cout << ("Finished Playback Test.");
 
 	std::ofstream csv_log;
 	csv_log.open("hand_guided_log.csv");
-	std::string csv_header_ = "joint_1,joint_2,joint_3,joint_4,joint_5,joint_6,joint_7,force_x,force_y,force_z,torque_x,torque_y,torque_z,time";
+	const std::string csv_header_ =
+		"joint_1,joint_2,joint_3,joint_4,joint_5,joint_6,joint_7,force_x,force_y,force_z,torque_x,torque_y,torque_z,time";
 	csv_log << csv_header_ << "\n";
-	for (int i = 0; i < record.first.size(); i++)
-		log(csv_log, record.first.at(i), record.second.at(i), i*0.001);
+	for (size_t i = 0; i < record.first.size(); i++)
+		log(csv_log, record.first.at(i), record.second.at(i), 0.001 * static_cast<double>(i));
 }
 
-void log(std::ofstream& csv_log, std::array<double, 7> j, std::array<double, 6> ft, double time) 
+void log(std::ofstream& csv_log, std::array<double, 7> j, std::array<double, 6> ft, double time)
 {
 	std::ostringstream j_log;
 	j_log << j[0] << "," << j[1] << "," << j[2] << "," << j[3] << "," << j[4] << "," << j[5] << "," << j[6];
@@ -185,9 +187,9 @@ void ptp_test(franka_proxy::franka_remote_interface& robot)
 	std::cout << "Starting PTP-Movement Test." << std::endl;
 
 	franka_proxy::robot_config_7dof pos1
-	{ {2.46732, -1.0536, -0.9351, -1.6704, 0.13675, 1.42062, 0.33471} };
+		{{2.46732, -1.0536, -0.9351, -1.6704, 0.13675, 1.42062, 0.33471}};
 	franka_proxy::robot_config_7dof pos2
-	{ {-0.00242, 1.236293, 2.465417, -1.26485, -0.00181, 1.914142, -1.06326} };
+		{{-0.00242, 1.236293, 2.465417, -1.26485, -0.00181, 1.914142, -1.06326}};
 
 	robot.set_speed_factor(0.2);
 	execute_retry([&] { robot.move_to(pos1); }, robot);
@@ -203,9 +205,9 @@ void force_test(franka_proxy::franka_remote_interface& robot)
 
 
 	franka_proxy::robot_config_7dof pos_with_scale
-	{ {1.09452, 0.475923, 0.206959, -2.33289, -0.289467, 2.7587, 0.830083} };
+		{{1.09452, 0.475923, 0.206959, -2.33289, -0.289467, 2.7587, 0.830083}};
 	franka_proxy::robot_config_7dof pos_above_table
-	{ {1.09703, 0.505084, 0.216472, -2.29691, -0.302112, 2.72655, 0.817159} };
+		{{1.09703, 0.505084, 0.216472, -2.29691, -0.302112, 2.72655, 0.817159}};
 	//{{1.10689, 0.660073, 0.240198, -2.03228, -0.33317, 2.63551, 0.784704}};
 
 	robot.set_speed_factor(0.2);
@@ -223,7 +225,7 @@ void impedance_admittance_tests_ermer_ba(franka_proxy::franka_remote_interface& 
 {
 	std::cout << "Starting Impedance - Hold Position Test." << std::endl;
 
-	std::array<double, 49> stiffness{ 0. };
+	std::array<double, 49> stiffness{0.};
 	stiffness[0] = stiffness[8] = stiffness[16] = stiffness[24] = 600.; // first four joints
 	stiffness[32] = 250.;
 	stiffness[40] = 150.;
@@ -236,9 +238,18 @@ void impedance_admittance_tests_ermer_ba(franka_proxy::franka_remote_interface& 
 	std::cout << "Starting Impedance - Follow Poses with Cartesian Impedance Test." << std::endl;
 	// positions
 	std::list<std::array<double, 16>> poses_for_cart = {
-		{0.321529, 0.8236, 0.467208, 0, 0.931889, -0.187754, -0.310343, 0, -0.167882, 0.53518, -0.827888, 0, 0.426976, 0.382873, 0.324984, 1},
-		{0.323711, 0.604326, 0.727999, 0, 0.698631, -0.671549, 0.246814, 0, 0.638056, 0.428714, -0.639601, 0, 0.600692, 0.372768, 0.415227, 1},
-		{0.826378, 0.559426, 0.0642109, 0, 0.562775, -0.824385, -0.0604543, 0, 0.0191152, 0.086096, -0.996103, 0, 0.503131, 0.2928, 0.296891, 1}
+		{
+			0.321529, 0.8236, 0.467208, 0, 0.931889, -0.187754, -0.310343, 0, -0.167882, 0.53518, -0.827888, 0,
+			0.426976, 0.382873, 0.324984, 1
+		},
+		{
+			0.323711, 0.604326, 0.727999, 0, 0.698631, -0.671549, 0.246814, 0, 0.638056, 0.428714, -0.639601, 0,
+			0.600692, 0.372768, 0.415227, 1
+		},
+		{
+			0.826378, 0.559426, 0.0642109, 0, 0.562775, -0.824385, -0.0604543, 0, 0.0191152, 0.086096, -0.996103, 0,
+			0.503131, 0.2928, 0.296891, 1
+		}
 	};
 
 	robot.cartesian_impedance_poses(poses_for_cart, 10, false, false, 50., 300.);
@@ -249,9 +260,18 @@ void impedance_admittance_tests_ermer_ba(franka_proxy::franka_remote_interface& 
 	std::cout << "Starting Impedance - Follow Poses with Joint Impedance Test." << std::endl;
 	// positions
 	std::list<std::array<double, 16>> poses_for_joint = {
-		{0.321529, 0.8236, 0.467208, 0, 0.931889, -0.187754, -0.310343, 0, -0.167882, 0.53518, -0.827888, 0, 0.426976, 0.382873, 0.324984, 1},
-		{0.323711, 0.604326, 0.727999, 0, 0.698631, -0.671549, 0.246814, 0, 0.638056, 0.428714, -0.639601, 0, 0.600692, 0.372768, 0.415227, 1},
-		{0.826378, 0.559426, 0.0642109, 0, 0.562775, -0.824385, -0.0604543, 0, 0.0191152, 0.086096, -0.996103, 0, 0.503131, 0.2928, 0.296891, 1}
+		{
+			0.321529, 0.8236, 0.467208, 0, 0.931889, -0.187754, -0.310343, 0, -0.167882, 0.53518, -0.827888, 0,
+			0.426976, 0.382873, 0.324984, 1
+		},
+		{
+			0.323711, 0.604326, 0.727999, 0, 0.698631, -0.671549, 0.246814, 0, 0.638056, 0.428714, -0.639601, 0,
+			0.600692, 0.372768, 0.415227, 1
+		},
+		{
+			0.826378, 0.559426, 0.0642109, 0, 0.562775, -0.824385, -0.0604543, 0, 0.0191152, 0.086096, -0.996103, 0,
+			0.503131, 0.2928, 0.296891, 1
+		}
 	};
 
 	robot.cartesian_impedance_poses(poses_for_joint, 10, false, false, 50., 300.);
