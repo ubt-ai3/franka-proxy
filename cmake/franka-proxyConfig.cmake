8958f767f5692574d2e22af3b28fb789d2374b0a
Johannes Hartwig <johannes.hartwig@uni-bayreuth.de>
2024-06-13 17:32:44 +0200

    fixed externals in cmake config

--- cmake/franka-proxyConfig.cmake
+++ cmake/franka-proxyConfig.cmake
@@ -1,8 +1,9 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(asio CONFIG)
-find_dependency(nlohmann_json CONFIG)
-find_dependency(Eigen3 CONFIG)
-find_dependency(Threads REQUIRED)
+find_package(argparse CONFIG REQUIRED)
+find_package(asio CONFIG REQUIRED)
+find_package(Eigen3 CONFIG REQUIRED)
+find_package(nlohmann_json REQUIRED)
+find_package(Threads REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/franka-proxyTargets.cmake")
