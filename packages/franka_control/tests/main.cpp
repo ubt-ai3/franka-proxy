dec343cd23e21faf5fe4ce57cfd7ef3c7426cc64
Johannes Hartwig <johannes.hartwig@uni-bayreuth.de>
2025-06-16 18:35:59 +0200

    cleanup: formatting for cpp control test

--- packages/franka_control/tests/main.cpp
+++ packages/franka_control/tests/main.cpp
@@ -12,7 +12,6 @@
 // todo: this sensor calibration should not be used from here
 #include "schunk_ft_to_franka_calibration.hpp"
 
-using namespace franka_proxy;
 
 void franka_controller_remote_test(const std::string& ip);
 void franka_controller_emulated_test();
@@ -36,9 +35,9 @@ int main(int argc, char* argv[])
 	       .default_value(std::string{"127.0.0.1"})
 	       .metavar("IP");
 
-	// todo: this sensor calibration should not be used from here
+	// todo: this sensor calibration should not be here
 	program.add_argument("-c", "--calibrate-fts")
-	       .help("specify ip for franka-proxy connection to calibrate fts")
+	       .help("specify ip for franka-proxy connection to calibrate schunk net-box fts")
 	       .default_value(std::string{"127.0.0.1"})
 	       .metavar("IP");
 
@@ -48,9 +47,9 @@ int main(int argc, char* argv[])
 	       .metavar("IP");
 
 	program.add_argument("-t", "--test-generic")
-		.help("generic test function")
-		.default_value(std::string("127.0.0.1"))
-		.metavar("IP");
+	       .help("generic test function")
+	       .default_value(std::string("127.0.0.1"))
+	       .metavar("IP");
 
 	try
 	{
@@ -152,13 +151,13 @@ void franka_controller_remote_test(const std::string& ip)
 
 	robot->move(joints_test);
 
-	Eigen::Affine3d pose(franka_proxy_util::fk(joints_test).back());
+	Eigen::Affine3d pose(franka_proxy::franka_proxy_util::fk(joints_test).back());
 	pose.linear()
 		<< 0.707107, 0.707107, 0,
 		0.707107, -0.707107, 0,
 		0, 0, -1;
 
-	const auto ik_solution = franka_proxy_util::ik_fast_closest(
+	const auto ik_solution = franka_proxy::franka_proxy_util::ik_fast_closest(
 		pose, franka_control::robot_config_7dof(joints_test));
 	robot->move(ik_solution);
 
@@ -175,7 +174,7 @@ void franka_controller_emulated_test()
 	const auto joints_test((franka_control::robot_config_7dof()
 		<< 1.08615, 0.044619, 0.227112, -2.26678, -0.059792, 2.27532, 0.605723).finished());
 
-	std::cout << "Status tests: Moving to test config and back.\n"; // todo: design a useful test function
+	std::cout << "Status tests: Moving to test config and back.\n";
 	std::atomic_bool stop(false);
 	std::thread t
 	([&stop, &robot]()
@@ -188,25 +187,26 @@ void franka_controller_emulated_test()
 	});
 	robot->move(joints_test);
 	robot->move(joints_start);
+
 	stop = true;
 	t.join();
 
 
-	std::cout << "Move tests: ... ";
+	std::cout << "Move test using IK: ... ";
 	robot->move(joints_test);
 
-	Eigen::Affine3d pose(franka_proxy_util::fk(joints_test).back());
+	Eigen::Affine3d pose(franka_proxy::franka_proxy_util::fk(joints_test).back());
 	pose.linear()
 		<< 0.707107, 0.707107, 0,
 		0.707107, -0.707107, 0,
 		0, 0, -1;
 
-	const auto ik_solution_joints = franka_proxy_util::ik_fast_closest(
+	const auto ik_solution_joints = 
+		franka_proxy::franka_proxy_util::ik_fast_closest(
 		pose, franka_control::robot_config_7dof(joints_test));
 	robot->move(ik_solution_joints);
-
-	[[maybe_unused]] bool target_reached = robot->move_until_contact(ik_solution_joints);
-	std::cout << "result unknown." << '\n'; // todo: design a useful test function
+	// todo: design a useful test function
+	std::cout << "result needs to be checked manually." << '\n'; 
 
 
 	std::cout << "Gripper tests: ... ";
@@ -215,13 +215,15 @@ void franka_controller_emulated_test()
 	robot->grasp_gripper();
 	[[maybe_unused]] bool gripper_grasped = robot->gripper_grasped();
 	robot->open_gripper();
-	std::cout << "result unknown." << '\n'; // todo: design a useful test function
+	// todo: design a useful test function
+	std::cout << "result needs to be checked manually." << '\n';
 
 
 	std::cout << "Speed tests: ... ";
 	robot->set_speed_factor(0.1);
 	[[maybe_unused]] double speed = robot->speed_factor();
-	std::cout << "result unknown." << '\n'; // todo: design a useful test function
+	// todo: design a useful test function
+	std::cout << "result needs to be checked manually." << '\n';
 }
 
 namespace
@@ -275,25 +277,28 @@ void guiding_mode_test(const std::string& ip)
 	}
 	franka_control::franka_update_task update_task(*robot);
 
-	std::chrono::seconds duration(20);
+	std::chrono::seconds duration(10);
 	//set to default before test
 	robot->set_guiding_mode(true, true, true, true, true, true, false);
 
-	std::cout << "Robot is now for 20 secs in guiding mode with guidable DOF (1,1,1,0,0,0) and elbow is false" << '\n';
+	std::cout << "Robot is now for " << duration
+		<< " in guiding mode with guidable DOF (1,1,1,0,0,0) and elbow is false" << '\n';
 	robot->set_guiding_mode(true, true, true, false, false, false, false);
 	std::this_thread::sleep_for(duration);
 
-	std::cout << "Robot is now for 20 secs in default guiding mode" << '\n';
+	std::cout << "Robot is now for " << duration
+		<< " in default guiding mode" << '\n';
 	robot->set_guiding_mode(true, true, true, true, true, true, false);
 	std::this_thread::sleep_for(duration);
 
-	std::cout << "Robot is now for 20 secs in guiding mode with guidable DOF (1,1,0,1,1,0) and elbow is false" << '\n';
+	std::cout << "Robot is now for " << duration
+		<< "  in guiding mode with guidable DOF (1,1,0,1,1,0) and elbow is false" << '\n';
 	robot->set_guiding_mode(true, true, false, true, true, false, false);
 	std::this_thread::sleep_for(duration);
 
 	//set back to default after test
 	robot->set_guiding_mode(true, true, true, true, true, true, false);
-	std::cout << "Finished guiding mode test" << '\n';
+	std::cout << "Finished guiding mode test." << '\n';
 }
 
 void generic_test_function(const std::string& ip)
@@ -306,24 +311,28 @@ void generic_test_function(const std::string& ip)
 	}
 	catch (const std::exception&)
 	{
-		std::cerr << "Could not connect to franka-proxy with IP " << ip << "." << std::endl;
+		std::cerr << "Could not connect to franka-proxy with IP "
+			<< ip << ".\n";
 		return;
 	}
 	franka_control::franka_update_task update_task(*robot);
 
-	std::cout << "Status tests: 5 seconds.\n"; // todo: design a useful test function
+	std::chrono::seconds duration(10);
+	std::cout << "Generic tests called: The status test runs for "
+		<< duration << ".\n";
 	std::atomic_bool stop(false);
 	std::thread t
 	([&stop, &robot]()
+	{
+		while (!stop)
 		{
-			while (!stop)
-			{
-				print_status(*robot);
-				std::this_thread::sleep_for(std::chrono::duration<double>(1));
-			}
-		});
+			print_status(*robot);
+			std::this_thread::sleep_for(
+				std::chrono::duration<double>(0.1));
+		}
+	});
 
-	std::this_thread::sleep_for(std::chrono::duration<double>(3));
+	std::this_thread::sleep_for(duration);
 	stop = true;
 	t.join();
 }
